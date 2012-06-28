bb97a6d5c2f8c62c1f47239077d3b01367b49bd8
Russ Frank <russ.frank@rutgers.edu>
2012-06-28 14:02:23 -0400

    readme fixes; more manual mocking

--- README.md
+++ README.md
@@ -3,4 +3,42 @@
 **tock** is a mocking framework for Titanium.  It loads up a `jsca` description
 of the Titanium API and creates a `Titanium` object with all of the proper
 objects and methods for testing code.  Every proxy is an emitter (as in Ti
-itself).
+itself).  Methods also emit an event `function::name` when invoked.
+
+This is a continuation of my exploration of the testing of programs written for the
+Titanium framework.  It's a [Zombie.js](http://zombie.labnotes.org/) style
+approach to the problem; rather than running inside of the emulators and 
+simulating events, we instead just simulate the environment.  This may prove
+useful for testing certain types of user interfaces and code.  I've found it
+helpful for mocking requests for testing my network code.
+
+## How to use it
+
+Install it like this
+
+```shell
+$ npm i tock
+```
+
+Then, use it like this
+
+```javascript
+require('tock');
+
+var xhr = Titanium.Network.createHTTPClient();
+var view = Ti.UI.createView();
+```
+
+You'll probably want to require() some of the code in your `Resources` folder.
+Currently I have my project setup with a `test` folder which has a couple tests;
+these require code out of Resources after requiring `tock`.  Then, I attempt
+requests, make assertions, etc.
+
+## Future ideas
+
+1. The `jsca` file has type information.  Add setters which assert types.
+2. Assert types of arguments.
+
+## License
+
+MIT.
--- tock.js
+++ tock.js
@@ -1,18 +1,27 @@
 var fs = require('fs');
-var data = JSON.parse(fs.readFileSync('./api.jsca'));
-var Emitter = require('events').EventEmitter;
+var data = JSON.parse(fs.readFileSync(__dirname + '/api.jsca'));
+var Emitter = require('eventemitter2').EventEmitter2;
+var util = require('util');
+var _ = require('underscore');
 
 // Modify Emitter's prototype to conform with what Ti does
 Emitter.prototype.addEventListener = Emitter.prototype.on;
-Emitter.prototype.removeEventListener = Emitter.prototype.off;
+Emitter.prototype.removeEventListener = Emitter.prototype.removeListener;
 Emitter.prototype.fireEvent = Emitter.prototype.emit;
 
 Titanium = Ti = {};
 
-function retrieve (list) {
+function retrieve (root, list) {
   var name = list.shift();
-  var current = Ti[name] = Ti[name] || Object.create(Emitter);
-  if (list.length > 0) return retrieve(list);
+  if (!root[name]) {
+    var newobj = {};
+    newobj.name = name;
+    _.extend(newobj, Emitter.prototype);
+    Emitter.call(newobj);
+    root[name] = newobj;
+  }
+  var current = root[name];
+  if (list.length > 0) return retrieve(current, list);
   else return current;
 }
 
@@ -23,9 +32,9 @@ function method (namespace, obj, name) {
   // factory
   if (name.indexOf('create') != -1) {
     obj[name] = function (props) {
-      var proto = retrieve(namespace.concat([name.slice(6)]));
-      var ret = Object.create(proto);
-      _.extend(ret, props);
+      var proto = retrieve(Ti, namespace.concat([name.slice(6)]));
+      var ret = {};
+      _.extend(ret, proto, props);
       return ret;
     };
 
@@ -39,8 +48,11 @@ function method (namespace, obj, name) {
     };
   }
 
-  // otherwise it's some method so just make it a noop
-  obj[name] = function () {};
+  // otherwise it's some method so just make it emit that this method was
+  // called
+  obj[name] = function () {
+    if (this.fireEvent) this.fireEvent('function::' + name, {});
+  };
 }
 
 data.types.forEach(function (item) {
@@ -49,12 +61,50 @@ data.types.forEach(function (item) {
   if (item.name.indexOf('Titanium.') == -1) return;
 
   var name = item.name.slice(9);
-  var namespace = name.split('.').slice(1);
-  var obj = retrieve(name.split('.'));
+  var namespace = name.split('.').slice(-1);
+  var obj = retrieve(Ti, name.split('.'));
   
   item.functions.forEach(function (fn) {
     method(namespace, obj, fn.name);
   });
+
+  item.properties.forEach(function (prop) {
+  });
 });
 
-console.dir(Ti);
+// Some manual stuff
+
+Ti.Platform ={
+  osname: 'android',
+  version: '6',
+  model: 'mock',
+  id: 'mock-id',
+  is24HourTimeFormat: function () { return false; },
+  Android: {API_LEVEL: 18},
+  displayCaps: {
+    platformWidth: 480,
+    platformHeight: 800,
+    dpi: 160
+  }
+};
+
+Ti.Network._requests = {};
+var old = Ti.Network.createHTTPClient;
+Ti.Network.createHTTPClient = function (spec) {
+  var xhr = old(spec);
+  xhr.open = function (method, url) {
+    xhr.method = method;
+    xhr.url = url;
+    Ti.Network._requests[url] = xhr;
+    xhr.fireEvent('function::open', arguments);
+  };
+
+  xhr.send = function (data) {
+    xhr.data = data;
+    xhr.fireEvent('function::send', arguments);
+  };
+
+  return xhr;
+};
+
+module.exports = Ti;
